23ed8a89bc8a19d79c619ab05f5d3497e40c7fb1
Camilo Dev <59340905+ordavo@users.noreply.github.com>
2026-05-23 00:20:00 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,10 +1,10 @@
 import os
 
 ODBC_DRIVER = os.getenv("ODBC_DRIVER", "{ODBC Driver 17 for SQL Server}")
-SERVER      = os.getenv("SQLSERVER", "DESKTOP-UOJSRMF")   # SERVER
+SERVER      = os.getenv("SQLSERVER", "")   # SERVER
 DATABASE    = os.getenv("SQLDB", "Tesis")
-USER        = os.getenv("SQLUSER", "sa")
-PASSWORD    = os.getenv("SQLPWD", "Guadual1t0")
+USER        = os.getenv("SQLUSER", "")
+PASSWORD    = os.getenv("SQLPWD", "")
 
 connection_string = (
     f"DRIVER={ODBC_DRIVER};"
